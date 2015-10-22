8064f245b3c01b146499b48874a5b09a0b34e614
Enrico Zini <enrico@enricozini.org>
2015-10-22 15:31:57 +0300

    Use overrides

--- provami/datagridmodel.h
+++ provami/datagridmodel.h
@@ -30,12 +30,12 @@ public:
     Model& dataModel() { return model; }
     const Model& dataModel() const { return model; }
     ColumnType resolveColumnType(int column) const;
-    int rowCount(const QModelIndex &parent) const;
-    int columnCount(const QModelIndex &parent) const;
-    QVariant data(const QModelIndex &index, int role) const;
-    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
-    Qt::ItemFlags flags(const QModelIndex& index) const;
-    bool setData(const QModelIndex& index, const QVariant& value, int role=Qt::EditRole);
+    int rowCount(const QModelIndex &parent) const override;
+    int columnCount(const QModelIndex &parent) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+    Qt::ItemFlags flags(const QModelIndex& index) const override;
+    bool setData(const QModelIndex& index, const QVariant& value, int role=Qt::EditRole) override;
     const Value* valueAt(const QModelIndex& index) const;
 
 signals:
